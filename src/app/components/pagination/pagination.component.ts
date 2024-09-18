cb7897fd80a6826b47e6d2208a3bdeb533367b40
MobentisCM <carlos.manso@mobentis.com>
2024-09-18 12:00:20 +0200

    cambio de paginacion con selector de items por pagina

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -12,47 +12,56 @@ import {
   styleUrls: ['./pagination.component.css'],
 })
 export class PaginationComponent implements OnChanges {
-  @Input() totalItems!: number; 
-  @Input() itemsPerPage!: number; 
-  @Input() currentPage: number = 1; 
+  @Input() totalItems: number = 0;
+  @Input() itemsPerPage: number = 10;
+  @Input() currentPage: number = 1;
 
-  @Output() pageChange = new EventEmitter<number>(); 
+  @Output() pageChange = new EventEmitter<number>();
+  @Output() itemsPerPageChanged = new EventEmitter<number>();
 
-  totalPages: number = 0; 
-  pages: (number | string)[] = []; 
-  maxVisiblePages: number = 7; 
+  totalPages: number = 0;
+  pages: (number | string)[] = [];
+  maxVisiblePages: number = 9;
+
+  itemsPerPageOptions: number[] = [5, 10, 20, 50]; // Opciones para el selector
+
+  ngOnInit() {
+    this.updatePagination();
+  }
 
   ngOnChanges() {
+    this.updatePagination();
+  }
+
+  updatePagination() {
     this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
     this.generatePages();
   }
 
   generatePages() {
     const pagesArray: (number | string)[] = [];
-    
+
     if (this.totalPages <= this.maxVisiblePages) {
       for (let i = 1; i <= this.totalPages; i++) {
         pagesArray.push(i);
       }
     } else {
-      const startPage = Math.max(2, this.currentPage - 1); 
-      const endPage = Math.min(this.totalPages - 1, this.currentPage + 1); 
+      const startPage = Math.max(3, this.currentPage - 2);
+      const endPage = Math.min(this.totalPages - 2, this.currentPage + 2);
 
       pagesArray.push(1);
 
       if (this.currentPage <= 3) {
-        for (let i = 2; i <= 5; i++) {
+        for (let i = 2; i <= 7; i++) {
           pagesArray.push(i);
         }
         pagesArray.push('...');
-      } 
-      else if (this.currentPage >= this.totalPages - 2) {
+      } else if (this.currentPage >= this.totalPages - 2) {
         pagesArray.push('...');
-        for (let i = this.totalPages - 4; i < this.totalPages; i++) {
+        for (let i = this.totalPages - 6; i < this.totalPages; i++) {
           pagesArray.push(i);
         }
-      } 
-      else {
+      } else {
         pagesArray.push('...');
         for (let i = startPage; i <= endPage; i++) {
           pagesArray.push(i);
@@ -63,13 +72,26 @@ export class PaginationComponent implements OnChanges {
       pagesArray.push(this.totalPages);
     }
 
-
     this.pages = pagesArray;
   }
 
   changePage(page: number | string) {
-    if (typeof page === 'number' && page > 0 && page <= this.totalPages) {
-      this.pageChange.emit(page); 
+    if (typeof page === 'number') {
+      if (page >= 1 && page <= this.totalPages) {
+        this.currentPage = page;
+        this.pageChange.emit(this.currentPage);
+        this.updatePagination();
+      }
+    }
+  }
+
+  onItemsPerPageChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    if (target) {
+      const newItemsPerPage = +target.value;
+      this.itemsPerPage = newItemsPerPage;
+      this.itemsPerPageChanged.emit(this.itemsPerPage);
+      this.updatePagination();
     }
   }
 }
